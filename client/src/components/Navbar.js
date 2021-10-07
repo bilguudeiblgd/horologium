9b98560b8e75f00fc89012304bcea2f3af965653
bilguudeiblgd <bilguudei.b8@gmail.com>
2021-10-07 14:03:11 +0800

    little bit mobile!

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,6 +6,7 @@ import { RiTimer2Line, RiDashboardLine } from "react-icons/ri";
 import { BsListNested } from 'react-icons/bs';
 import { FaReact } from 'react-icons/fa';
 import { FaRegUserCircle } from 'react-icons/fa';
+import {isMobile} from 'react-device-detect';
 
 
 import "./Navbar.css"
@@ -15,8 +16,48 @@ export default class Navbar extends Component {
 
     return (
       <div className="all-navbar-container">
+        {isMobile ? <div className="mobile-nav-container">
+          
+          <ul>
+            
+            <li class="mobile-list-container">
+              <Link to="/" className="list-container"><div>
+                <RiTimer2Line className="nav-icons" />
+              </div>
+                </Link>
+            </li>
+            <li class="mobile-list-container">
+              <Link to="/subjects" className="list-container"><div>
+                <BsListNested className="nav-icons" />
+              </div>
+                </Link>
+            </li>
+            <li class="mobile-list-container">
+              <Link to="/subjects/add" className="list-container"><div><RiDashboardLine className="nav-icons" /></div></Link>
+            </li>
+          </ul>
+          <div className="login" style={{display: 'flex', flexDirection: 'column'}}>
+            <button onClick={this.props.login} style={{ display: 'flex', flexDirection: 'column', 'alignItems': 'center', outline: 'none', backgroundColor: 'transparent', border: 'none' }}>
+              {/* <p style={{ color: "white" }}>{this.props.username}</p> */}
+              
+              {
+                this.props.loggedIn ? <div style={{width: "50px", height: "50px", border: "1px white solid", borderRadius: '8px'}}><img src={`https://avatars.dicebear.com/api/human/${this.props.username}.svg`}  alt ="Your sprite"/></div>
+              :
+              <FaRegUserCircle className="mobile-login-icon" />
+            }
+            </button>
+            {this.props.loggedIn ?
+              <button style={{ marginTop: '8px', background: 'transparent', color: 'grey', outline: 'none', border: 'none' }} onClick={this.props.logout}>
+                Logout
+              </button>
+              : ''}
 
+          </div>
+        </div> 
+        // Desktop
+        :
         <div className="nav-container">
+          
           <ul>
             <li style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
               <FaReact style={{ fontSize: "75px", marginBottom: "10pxtc", color: "white" }} />
@@ -58,7 +99,7 @@ export default class Navbar extends Component {
 
           </div>
         </div>
-
+  }
       </div>
 
     );
